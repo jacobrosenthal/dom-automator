5c8f1db908f36a9926a5548f0fe65492eea741a0
Jacob Rosenthal <jacobrosenthal@gmail.com>
2023-05-12 18:03:34 -0700

    feat: stub out more, notice we cant feed back observations

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,54 @@ type Action = {
 type Agent = {
   actions: AsyncIterator<Action>
 }
-
-export const analyze = (
-  domString: string,
-  options?: Record<string, any>
-): string => {
-  return null
+type Step = {
+  action: string
+  // observation:string
 }
 
-export const createAgent = async (
-  prompt: string,
+const completion = async (
+  dom: string,
+  task: string,
+  previousSteps: Step[]
+): Promise<string> => {
+  await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: `you an an agent. Your task is to ${task}\nthis is the dom:\n${dom}\n${previousSteps.toString()}`
+      }
+    ]
+  })
+  return 'something from result'
+}
+
+const parse = (result: string): Action => {
+  return JSON.parse(result) as Action
+}
+
+export const createAgent = (
+  task: string,
   getDOM: () => Promise<string>,
   options?: Record<string, any>
 ): Agent => {
-  const startDOM = await getDOM()
-  return null
+  async function* actionGenerator(): AsyncIterableIterator<Action> {
+    const steps: Step[] = []
+    while (true) {
+      const result = await completion(task, await getDOM(), steps)
+      const action = parse(result)
+      steps.push({ action: result })
+      // typically you would run the tool here, and if what it gave you doesnt parse or isnt a correct tool use you would add the error to the tool use log and rerun the completion again so it can self correct
+      // We could sink running the tool inside the generator here, but at that point, why poll a generator to completion?
+      yield action
+    }
+  }
+
+  async function* actions(): AsyncIterableIterator<Action> {
+    yield* actionGenerator()
+  }
+
+  return { actions: actions() }
 }
 
 export const createActionScript = (
